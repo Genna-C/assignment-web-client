cd303b9d91ee5a066412dc5e0f356fae64d84ad2
Genna-C <genna@ualberta.ca>
2023-10-13 08:55:42 -0600

    GET and POST functional

--- httpclient.py
+++ httpclient.py
@@ -21,8 +21,11 @@
 import sys
 import socket
 import re
+import json
 # you may use urllib to encode data appropriately
-import urllib.parse
+from urllib.parse import urlparse, urlencode
+
+# These are used if Port and Host are not specified 
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -41,13 +44,18 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        top = data.split("\r\n")[0]  
+        return int(top.split(" ")[1])
 
     def get_headers(self,data):
-        return None
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0', "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7", "Accept-Language" : "en-US,en;q=0.9", "Accept-Encoding" : "gzip, deflate, br", "Upgrade-Insecure-Requests" : "1"}
+        for header in headers:
+            data += f"{header}: {headers[header]}\r\n"    
+        data += f"\r\n"
+        return data
 
     def get_body(self, data):
-        return None
+        return data.split("\r\n\r\n")[1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -55,6 +63,23 @@ class HTTPClient(object):
     def close(self):
         self.socket.close()
 
+    def split_host(self, url):
+        split_url = urlparse(url) 
+        HOST = split_url.hostname
+        PORT = split_url.port
+        if not PORT: PORT = 80
+        return HOST, PORT
+        
+    def validate_url(self, url):
+        parsed_url = urlparse(url) 
+        add_url = parsed_url.scheme + parsed_url.netloc
+        if parsed_url.scheme == "http": 
+            add_url = "https://" + parsed_url.netloc
+        
+        if len(urlparse(url).path) > 1: 
+             add_url += parsed_url.path
+        return add_url 
+            
     # read everything from the socket
     def recvall(self, sock):
         buffer = bytearray()
@@ -68,14 +93,39 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        HOST, PORT = self.split_host(url)
+        self.connect(HOST, PORT) 
+        body = f"GET {urlparse(url).path} HTTP/1.1\r\nHost: {HOST}\r\n" 
+        body = self.get_headers(body) 
+ 
+        self.sendall(body) 
+        response = self.recvall(self.socket) 
+        self.close() 
+        
+        print("------------")
+        print("for request %s " %body) 
+        print("Printing Response: %s" %self.get_headers(response)) 
+        print("------------")
+        
+        return HTTPResponse(self.get_code(response), self.get_body(response))
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        HOST, PORT = self.split_host(url)
+        self.connect(HOST, PORT)
+        request = "POST / HTTP/1.1\r\nHost: %s\r\n\Content-Type: application/x-www-form-url-encoded\r\nContent-Length: " %url
+
+        if args: 
+            length = len(urlencode(args)) 
+            request += "%s\r\n\r\n" %length
+            request += "{0}\r\n\r\n".format(urlencode(args))
+        else:
+            request += "0\r\n\r\n"
+            
+        self.socket.sendall(request.encode("utf-8")) 
+        response = self.recvall(self.socket) 
+ 
+        self.close()
+        return HTTPResponse(self.get_code(response), self.get_body(response))
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
